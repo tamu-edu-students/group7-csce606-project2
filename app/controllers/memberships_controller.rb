de740d485701b230f72ee42a392087d215670196
Kate Brown <katelyn.brown@tamu.edu>
2025-11-02 16:29:16 -0600

    Unread messages can be cleared. Notifications link to corresponding project / teaching offer.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,7 +1,7 @@
 class MembershipsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_memberable
-  before_action :authorize_tutor!, only: [ :index, :approve ]
+  before_action :authorize_tutor!, only: [:index, :approve]
 
   # Learner requests to join
   def create
@@ -11,7 +11,8 @@ class MembershipsController < ApplicationController
       Notification.create!(
         user: @memberable.author,
         notifiable: membership,
-        message: "#{current_user.name} has requested to join your #{@memberable.class.name.demodulize.downcase}."
+        message: "#{current_user.name} has requested to join your #{memberable_name}.",
+        url: memberable_path(@memberable)
       )
       @memberable.memberships.reload
       redirect_back fallback_location: root_path, notice: "Join request sent!"
@@ -29,12 +30,15 @@ class MembershipsController < ApplicationController
   def approve
     membership = @memberable.memberships.find(params[:id])
     membership.update(status: "approved")
+
     Notification.create!(
-        user: membership.user,
-        notifiable: membership,
-        message: "Your request to join #{@memberable.class.name.demodulize.downcase} '#{@memberable.title}' has been approved!"
+      user: membership.user,
+      notifiable: membership,
+      message: "Your request to join #{memberable_name} '#{@memberable.title}' has been approved!",
+      url: memberable_path(@memberable)
     )
-    redirect_back fallback_location: teaching_offer_memberships_path(@memberable),
+
+    redirect_back fallback_location: memberable_memberships_path(@memberable),
                   notice: "#{membership.user.name} has been approved."
   end
 
@@ -44,13 +48,15 @@ class MembershipsController < ApplicationController
 
     if current_user == membership.memberable.author
       membership.update(status: "rejected")
+
       Notification.create!(
         user: membership.user,
         notifiable: membership,
-        message: "Your request to join #{@memberable.class.name.demodulize.downcase} '#{@memberable.title}' has been rejected."
+        message: "Your request to join #{memberable_name} '#{@memberable.title}' has been rejected.",
+        url: memberable_path(@memberable)
       )
 
-      redirect_back fallback_location: teaching_offer_path(membership.memberable),
+      redirect_back fallback_location: memberable_path(@memberable),
                     notice: "#{membership.user.name}'s request has been rejected."
     else
       redirect_back fallback_location: root_path, alert: "You don't have permission to do that."
@@ -61,8 +67,14 @@ class MembershipsController < ApplicationController
   def destroy
     membership = @memberable.memberships.find_by(user: current_user)
     if membership
+      Notification.create!(
+        user: @memberable.author,
+        notifiable: membership,
+        message: "#{current_user.name} has left your #{memberable_name}.",
+        url: memberable_path(@memberable)
+      )
       membership.destroy!
-      redirect_back fallback_location: root_path, notice: "You left this #{memberable_name.downcase}."
+      redirect_back fallback_location: root_path, notice: "You left this #{memberable_name}."
     else
       redirect_back fallback_location: root_path, alert: "Membership not found."
     end
@@ -85,6 +97,30 @@ class MembershipsController < ApplicationController
   end
 
   def memberable_name
-    @memberable.class.name.demodulize
+    @memberable.class.name.demodulize.downcase
+  end
+
+  # Dynamic path helper based on memberable type
+  def memberable_path(memberable)
+    case memberable
+    when TeachingOffer
+      teaching_offer_path(memberable)
+    when Project
+      project_path(memberable)
+    else
+      root_path
+    end
+  end
+
+  # Dynamic path helper for memberships index
+  def memberable_memberships_path(memberable)
+    case memberable
+    when TeachingOffer
+      teaching_offer_memberships_path(memberable)
+    when Project
+      project_memberships_path(memberable)
+    else
+      root_path
+    end
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,5 +3,14 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
+    @unread_count = current_user.notifications.unread.count
+    @notifications.unread.update_all(read: true)
   end
+
+  def notification_link
+  notification = current_user.notifications.find(params[:id])
+  target = notification.notifiable.respond_to?(:memberable) ? notification.notifiable.memberable : notification.notifiable
+  redirect_to polymorphic_path(target)
+end
+
 end
